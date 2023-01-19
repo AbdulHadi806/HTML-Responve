89560582f7f83d3479f1236cb758bc833078370d
Abdul Hadi <abdul.806@yahoo.com>
2023-01-20 02:29:49 +0500

    first commit

---
+++ js/jquery.js
@@ -0,0 +1,21 @@
+
+// $('html, body').ready(function () {
+//     $(".testimonial-button").click(function () {
+//         alert("Page not avaliable");
+//     });
+// })
+// $(".shop").click(function () {
+//     $('html, body').animate({
+//         scrollTop: $(".black-journal-sale").offset().top
+//     }, 1000);
+// });
+// $(".blog").click(function () {
+//     $('html, body').animate({
+//         scrollTop: $(".how-work-row").offset().top
+//     }, 1000);
+// });
+// $(".support").click(function () {
+//     $('html, body').animate({
+//         scrollTop: $(".footer").offset().top
+//     }, 1000);
+// });
